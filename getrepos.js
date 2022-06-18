38e318ed1852e3ceff8ce524f36f3129767470aa
flippyboi <flippybeats@gmail.com>
2022-06-18 11:49:54 +0300

    small improvements

--- getrepos.js
+++ getrepos.js
@@ -10,7 +10,7 @@ function requestUserRepos(username) {
         for (let i in data) {
             const createItem = document.createElement("div");
             createItem.className = 'repo-item';
-            createItem.innerHTML = `<div>${data[i].name}</div> ` + `<div>${data[i].description}</div>` + `<a href=${data[i].html_url}>Link</a> `;
+            createItem.innerHTML = `<a href=${data[i].html_url}>${data[i].name}</a> ` + `<div>${data[i].description}</div>`;
             list.append(createItem);
             console.log('Repo:', data[i].name);
             console.log('Description:', data[i].description);
@@ -27,7 +27,9 @@ requestUserRepos('flippyboi');
 function getUsername() {
     const username = document.getElementById("check").value;
     const header = document.querySelector('.usrname');
-    header.textContent = `-${username}-`;
-    document.querySelectorAll(".repo-item").forEach(e => e.remove());
-    requestUserRepos(username);
+    if (username != "") {
+        header.textContent = `-${username}-`;
+        document.querySelectorAll(".repo-item").forEach(e => e.remove());
+        requestUserRepos(username);
+    }
 }
